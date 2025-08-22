7bf3eacad90e0f2b1607202e07d10c3270a55b50
Aasray Sundar <aasray@gmail.com>
2025-08-22 09:34:45 +0530

    Updated Scanner

--- src/scanner.c
+++ src/scanner.c
@@ -188,10 +188,19 @@ Token scanToken() {
         case ';': return makeToken(TOKEN_SEMICOLON);
         case ',': return makeToken(TOKEN_COMMA);
         case '.': return makeToken(TOKEN_DOT);
-        case '-': return makeToken(TOKEN_MINUS);
-        case '+': return makeToken(TOKEN_PLUS);
-        case '/': return makeToken(TOKEN_SLASH);
-        case '*': return makeToken(TOKEN_STAR);
+        case '-': return makeToken(
+            match('-') ? TOKEN_DECREMENT : 
+                    (match('=') ? TOKEN_MINUS_EQUAL : TOKEN_MINUS));
+        case '+': return makeToken(
+                match('+') ? TOKEN_INCREMENT : 
+                    (match('=') ? TOKEN_PLUS_EQUAL : TOKEN_PLUS));
+        case '/': return makeToken(
+            match('/') ? TOKEN_SLASH_SLASH : 
+                    (match('=') ? TOKEN_SLASH_EQUAL : TOKEN_SLASH));
+        case '*': return makeToken(
+            match('*') ? TOKEN_STAR_STAR : 
+                    (match('=') ? TOKEN_STAR_EQUAL : TOKEN_STAR));
+        //TODO: CHECK THE || and && work
         case '|': 
             if(match('|')) return makeToken(TOKEN_OR);
             break;
