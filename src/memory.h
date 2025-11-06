84a7351d4690d4b067d9dd1f5f459ec83cc54862
Aasray Sundar <aasray@gmail.com>
2025-11-06 12:11:49 +0000

    Clox memory.h

--- src/memory.h
+++ src/memory.h
@@ -2,7 +2,8 @@
 #define graphiC_memory_h
 
 #include "common.h"
-// #include "object.h"
+#include "object.h"
+#include "value.h"
 
 #define ALLOCATE(type, count)\
     (type*)reallocate(NULL, 0, sizeof(type) * (count))     
@@ -20,6 +21,9 @@
 #define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void markObject(Obj* object);
+void markValue(Value value);
+void collectGarbage();
 void freeObjects();
 
 #endif
