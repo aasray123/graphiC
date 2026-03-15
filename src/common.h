707d45caa2b969bb828c6736d8b8564fa38f909b
Aasray Sundar <aasray@gmail.com>
2026-03-15 15:18:09 +0000

    Circle Script

--- src/common.h
+++ src/common.h
@@ -6,13 +6,13 @@
 #include <stdint.h>
 #include <string.h>
 
-// #define DEBUG_PRINT_CODE 
+#define DEBUG_PRINT_CODE 
 // #define DEBUG_TRACE_EXECUTION
 
 // #define DEBUG_STRESS_GC
 // #define DEBUG_LOG_GC
 
-#define DEBUG_MINOR_GC
+// #define DEBUG_MINOR_GC
 
 #define DEBUG_LOG_TIME
 #define UINT8_COUNT (UINT8_MAX + 1)
